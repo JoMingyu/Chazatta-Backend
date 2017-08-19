03428b18d6f2f3702f7b9e7c93de1be590fe382a
PlanB <city7310@naver.com>
2017-08-19 19:00:22 +0900

    아이디어 추가, 아이디어 개발 종료일 갖고노는거 완료

---
+++ project/Route/idea.js
@@ -0,0 +1,61 @@
+const router = require('express').Router();
+const mysql = require('../Database/mysql');
+const fcm = require('../Support/fcm');
+const util = require('util');
+
+setInterval(() => {
+    const date = new Date().toISOString().replace(/T/, ' ').replace(/\..+/, '').split(" ")[0];
+    mysql.query('SELECT owner, title FROM idea WHERE develop_end_date=?', [date], (err, rows) => {
+        for(idx in rows) {
+            let row = rows[idx];
+            mysql.query('SELECT registration_id FROM account WHERE access_token=?', [row.owner], (err, accountRows) => {
+                let ideaTitle = row.title;
+                let registrationId = accountRows[0].registration_id;
+                fcm.fcm(registrationId, `'${ideaTitle}' 아이디어의 개발 종료일`, '프로젝트를 완료하고 팀원을 평가해주세요!');
+            });
+        }
+    });
+}, 1000 * 3600 * 24);
+
+router.route('/idea').post((req, res) => {
+    // 아이디어 추가
+    let client = req.body.access_token;
+    let title = req.body.title;
+    let summary = req.body.summary;
+    let platform = req.body.platform;
+    let purpose = req.body.purpose;
+    let detail = req.body.detail;
+    let startDate = req.body.start_date;
+    let endDate = req.body.end_date;
+    let teamMaxCount = req.body.team_max_count;
+    let teamDesire = req.body.team_desire;
+
+    mysql.query('INSERT INTO idea(owner, title, summary, platform, purpose, detail, develop_start_date, develop_end_date, team_max_count, team_desire',
+        [
+            client,
+            title,
+            summary,
+            platform,
+            purpose,
+            detail,
+            startDate,
+            endDate,
+            teamMaxCount,
+            teamDesire
+        ]
+    );
+}).get((req, res) => {
+    // 아이디어 리스트 가져오기
+}).delete((req, res) => {
+    // 아이디어 삭제
+});
+
+router.route('/idea/detail').get((req, res) => {
+    // 아이디어 세부 정보
+});
+
+router.route('/idea/complete').post((req, res) => {
+    // 프로젝트 완료
+});
+
+module.exports = router;
--- project/app.js
+++ project/app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', require('./Route/account'));
+app.use('/', require('./Route/idea'));
 
 app.listen(8190, () => {
     console.log('Server is listening on 8190');
