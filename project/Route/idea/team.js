bab80da9c567ed0ac16f0a478f97d6c29cde0c30
PlanB <city7310@naver.com>
2017-08-20 04:40:56 +0900

    legacy 제거, 팀원 신청 목록 조회 구현

--- project/Route/idea/team.js
+++ project/Route/idea/team.js
@@ -8,8 +8,8 @@ router.route('/idea/team/applies').post((req, res) => {
 
     mysql.query('SELECT applier FROM idea_team WHERE idea_idx=?', [idx], (err, rows) => {
         let applier = JSON.parse(rows[0].applier);
-        for(let i = 0; i < applier.length; i++) {
-            if(applier[i] == email) {
+        for (let i = 0; i < applier.length; i++) {
+            if (applier[i] == email) {
                 // 중복 체크
                 res.sendStatus(204);
                 return;
@@ -26,6 +26,26 @@ router.route('/idea/team/applies').post((req, res) => {
     });
 }).get((req, res) => {
     // 지 아이디어에 대한 팀원 신청 목록 조회
+    let email = req.query.email;
+
+    let response = new Array();
+    mysql.query('SELECT idx, title FROM idea WHERE owner=?', [email], (err, rows) => {
+        for (let i = 0; i < rows.length; i++) {
+            let idx = rows[i].idx;
+            mysql.query('SELECT applier FROM idea_team WHERE idea_idx=?', [idx], (err, applierRows) => {
+                if (JSON.parse(applierRows[0].applier).length > 0) {
+                    response.push({
+                        idx: idx,
+                        title: rows[i].title,
+                        applier: applierRows[0].applier
+                    });
+                }
+                if (i == rows.length - 1) {
+                    res.send(response);
+                }
+            });
+        }
+    });
 });
 
 router.route('/idea/team/accept').post((req, res) => {
@@ -36,12 +56,12 @@ router.route('/idea/team/accept').post((req, res) => {
     mysql.query('SELECT applier, team FROM idea_team WHERE idea_idx=?', [idx], (err, rows) => {
         let appliers = JSON.parse(rows[0].applier);
         let team = JSON.parse(rows[0].team);
-        for(let i = 0; i < appliers.length; i++) {
-            if(appliers[i] == email) {
+        for (let i = 0; i < appliers.length; i++) {
+            if (appliers[i] == email) {
                 team.push(email);
                 appliers.splice(i, 1);
                 mysql.query('UPDATE idea_team SET team=?, applier=? WHERE idea_idx=?', [JSON.stringify(team), JSON.stringify(appliers), idx], (err, rows) => {
-                    if(!err) {
+                    if (!err) {
                         res.sendStatus(200);
                     } else {
                         res.sendStatus(204);
