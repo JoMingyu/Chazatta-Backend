feb355ff5ada6cd96b59dd4deb97dc2dd21d77b6
origin <syeuty@naver.com>
2017-08-19 16:09:51 +0900

    function profile

--- project/app.js
+++ project/app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-
+const router = require('./Route/router');
 const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/', router);
 
 app.listen(3000, () => {
     console.log('Server is listening on 3000');
